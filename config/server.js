f6dc492c366955bb1be62f12d6611061e260bf38
chetoui hamza <okuninoshi>
2021-04-05 21:07:21 +0100

    init project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6e51d8a981ba238b35f8aa976f20964a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c204f02f-ed98-4d73-8ff5-02935535361e'
+};
